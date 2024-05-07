db8e7af63326b0350aa9abe51ddc2a7a340d2d0b
lelalela <lelalela@daum.net>
2024-05-07 23:37:29 +0900

    add Tests

---
+++ src/test/java/com/polarbookshop/catalogservice/BookValidationTests.java
@@ -0,0 +1,37 @@
+package com.polarbookshop.catalogservice;
+
+import com.polarbookshop.catalogservice.domain.Book;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.*;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BookValidationTests {
+
+    private static Validator validator;
+
+    @BeforeAll
+    static void setup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void whenAllFieldsCorrectThenValidationSucceeds(){
+        var book = new Book("1234567891", "Title",  "Author", 9.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isEmpty();
+    }
+
+    @Test
+    void whenIsbnDefinedButIncorrectThenValidationFails(){
+        var book = new Book("1234567891", "Title",  "Author", 9.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        assertThat(violations.iterator().next().getMessage()).isEqualTo("The ISBN format must be valid");
+    }
+
+}
